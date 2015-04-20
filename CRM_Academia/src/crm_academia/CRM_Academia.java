722fce5f8445537424159010e42e8e042bfb7d7c
RODOLFO <rodolfo_eliezer@hotmail.com>
2015-04-20 10:43:35 -0300

    Somente um teste.

--- CRM_Academia/src/crm_academia/CRM_Academia.java
+++ CRM_Academia/src/crm_academia/CRM_Academia.java
@@ -15,7 +15,7 @@ public class CRM_Academia {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Nois vão ficar Fico");
     }
     
 }
