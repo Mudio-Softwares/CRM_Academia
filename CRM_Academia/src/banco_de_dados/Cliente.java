0958ac44a6200cd62646f2d3a65759987d6118e7
RODOLFO <rodolfo_eliezer@hotmail.com>
2015-04-30 21:04:33 -0300

    Start on programming

---
+++ CRM_Academia/src/banco_de_dados/Cliente.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco_de_dados;
+
+abstract public class Cliente {
+    private String nome, email, num_telefone;
+    private double matricula;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNum_telefone() {
+        return num_telefone;
+    }
+
+    public void setNum_telefone(String num_telefone) {
+        this.num_telefone = num_telefone;
+    }
+
+    public double getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(double matricula) {
+        this.matricula = matricula;
+    }
+}
